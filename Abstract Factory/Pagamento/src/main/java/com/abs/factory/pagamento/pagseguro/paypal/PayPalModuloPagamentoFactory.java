94d318c5de879465bc3262e8e355ad27cb613ec7
Leo <161379464+Leoocx@users.noreply.github.com>
2025-05-10 10:09:49 -0300

    Add files via upload

---
+++ Abstract Factory/Pagamento/src/main/java/com/abs/factory/pagamento/pagseguro/paypal/PayPalModuloPagamentoFactory.java
@@ -0,0 +1,21 @@
+package com.abs.factory.pagamento.paypal;
+
+import com.abs.factory.gestorderisco.ClearSale;
+import com.abs.factory.gestorderisco.GestorDeRisco;
+import com.abs.factory.operadora.Operadora;
+import com.abs.factory.operadora.Redecard;
+import com.abs.factory.pagamento.ModuloPagamentoFactory;
+
+public class PayPalModuloPagamentoFactory implements ModuloPagamentoFactory {
+
+
+    @Override
+    public Operadora criarOperadora() {
+        return new Redecard();
+    }
+
+    @Override
+    public GestorDeRisco criarGestorDeRisco() {
+        return new ClearSale();
+    }
+}
